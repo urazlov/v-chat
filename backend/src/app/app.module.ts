aafc7750687ff96a2f404292d822f17f68a20092
Viktor <urazlov.@gmail.com>
2024-04-30 17:03:58 +0400

    init project

---
+++ backend/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'v-chat-user',
+      password: 'v-chat-password',
+      database: 'v-chat-database',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
--- frontend/src/app/nx-welcome.component.ts
+++ frontend/src/app/nx-welcome.component.ts
@@ -432,7 +432,7 @@ import { CommonModule } from '@angular/common';
         <div id="welcome">
           <h1>
             <span> Hello there, </span>
-            Welcome v-chat 👋
+            Welcome frontend 👋
           </h1>
         </div>
         <!--  HERO  -->
@@ -836,7 +836,7 @@ nx g &#64;nx/angular:component ui/src/lib/button</pre>
               </svg>
               View project details
             </summary>
-            <pre>nx show project v-chat --web</pre>
+            <pre>nx show project frontend --web</pre>
           </details>
           <details>
             <summary>
